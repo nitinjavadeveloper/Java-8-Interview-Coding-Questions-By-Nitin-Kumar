8d55c95ebdf12620263ace5cf3e3f45f7cb3d04f
NK119738 <nitinjavadeveloper@users.noreply.github.com>
2025-09-02 16:31:46 +0530

    Oops

---
+++ patterns/Pattern_1.java
@@ -0,0 +1,20 @@
+/* 
+1
+1 2
+1 2 3
+ */
+
+
+class Pattern_1 {
+
+	public static void main(String[] args) {
+	
+	for (int i = 1; i<=5; i++) {
+		 for (int j = 1 ; j <= i; j++ ) {
+			 
+			 System.out.print(j+" ");
+		 }
+		 System.out.println();
+		}
+	}
+}
